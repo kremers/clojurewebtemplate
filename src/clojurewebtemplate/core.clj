1a14b9ab772b5409043ffbee12f85244b813710f
kremers <info@martinkremers.de>
2013-12-14 21:43:37 +0100

    getting simple hello world working

--- src/clojurewebtemplate/core.clj
+++ src/clojurewebtemplate/core.clj
@@ -1,6 +1,28 @@
-(ns clojurewebtemplate.core)
+(ns clojurewebtemplate.core
+  (:gen-class)
+  (:require (compojure [handler :as handler]))
+  (:use [compojure core]
+        [clojure.tools.logging :only (info debug error)]
+        [org.httpkit.server]
+        [ring.middleware content-type file file-info params reload]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes sync_routes
+  (GET  "/" [] "Hello World!")
+  (ANY  "*" [] {:status 404 :body "sorry, 404"}))
+
+(def siteconfig
+  (-> sync_routes
+      (wrap-file "resources")
+      wrap-file-info
+      handler/site))
+
+(defn -main [& args]
+  (if-let [port (Integer/parseInt (first args))]
+    (do (info "starting http-endpoint...")
+        (run-server siteconfig {:port port})
+        (info (str "|- http-endpoint ready (" port ")")))))
+
+
+
+
+
